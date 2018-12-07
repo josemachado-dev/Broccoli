efea2a33b3e441592194b7b87f786206aee8a395
José Machado <33759343+josemachado-dev@users.noreply.github.com>
2018-12-07 16:06:45 +0000

    Added LICENSE to main.py

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+#############################################################################
+#[not yet named project] aims to be a tool that serves as a bridge          #
+#       between game writers and programmers                                #
+#   Copyright (C) 2018  José Machado                                        #
+#                                                                           #
+#    This program is free software: you can redistribute it and/or modify   #
+#    it under the terms of the GNU General Public License as published by   #
+#    the Free Software Foundation, either version 3 of the License, or      #
+#    (at your option) any later version.                                    #
+#                                                                           #
+#    This program is distributed in the hope that it will be useful,        #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of         #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          #
+#    GNU General Public License for more details.                           #
+#                                                                           #
+#    You should have received a copy of the GNU General Public License      #
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>. #
+#############################################################################
+
 #External libraries
 import tkinter as tk
 import tkinter.filedialog
