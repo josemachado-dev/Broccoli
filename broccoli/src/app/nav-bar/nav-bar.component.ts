e939abf0a42f89bc5b4e6ce17d46fe9e603780db
josemachado-dev <josemachado.dev@gmail.com>
2019-07-07 13:26:56 +0100

    Changes to Navbar
    Small changes to navbar, to start to make it more user friendly

--- broccoli/src/app/nav-bar/nav-bar.component.ts
+++ broccoli/src/app/nav-bar/nav-bar.component.ts
@@ -5,16 +5,4 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent {
-
-  constructor() { }
-
-  reportBug(){
-    window.open("https://github.com/josemachado-dev/improved-broccoli/issues/new/choose", "_blank");
-  }
-
-  requestFeature(){
-    window.open("https://github.com/josemachado-dev/improved-broccoli/issues/new/choose", "_blank");
-  }
-
-}
+export class NavBarComponent {  constructor() { } }
